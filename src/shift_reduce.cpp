c73e1c13971465c67880295706719a6047a2717f
Rustam Subkhankulov <subkhankulov@ispras.ru>
2024-04-24 04:02:43 +0300

    almost done with sub, num and div

--- src/shift_reduce.cpp
+++ src/shift_reduce.cpp
@@ -49,68 +49,118 @@ using namespace Grammar;
 const Shift_reduce_parser::Action_table Shift_reduce_parser::action_table = {
 
   { {.state = 0U,  .symb = ID    }, { .type = SHIFT,  .num = 5U } },
+  { {.state = 0U,  .symb = NUM   }, { .type = SHIFT,  .num = 12U} }, // 
   { {.state = 0U,  .symb = OP_BR }, { .type = SHIFT,  .num = 4U } },
 
   { {.state = 1U,  .symb = ADD   }, { .type = SHIFT,  .num = 6U } },
+  { {.state = 1U,  .symb = SUB   }, { .type = SHIFT,  .num = 13U} }, //
   { {.state = 1U,  .symb = END   }, { .type = ACCEPT            } },
 
   { {.state = 2U,  .symb = ADD   }, { .type = REDUCE, .num = 3U } },
+  { {.state = 2U,  .symb = SUB   }, { .type = REDUCE, .num = 2U } }, //
   { {.state = 2U,  .symb = MUL   }, { .type = SHIFT,  .num = 7U } },
+  { {.state = 2U,  .symb = DIV   }, { .type = SHIFT,  .num = 15U} }, // 
   { {.state = 2U,  .symb = CL_BR }, { .type = REDUCE, .num = 3U } },
   { {.state = 2U,  .symb = END   }, { .type = REDUCE, .num = 3U } },
 
   { {.state = 3U,  .symb = ADD   }, { .type = REDUCE, .num = 6U } },
+  { {.state = 3U,  .symb = SUB   }, { .type = REDUCE, .num = 6U } }, //
   { {.state = 3U,  .symb = MUL   }, { .type = REDUCE, .num = 6U } },
+  { {.state = 3U,  .symb = DIV   }, { .type = REDUCE, .num = 6U } }, //
   { {.state = 3U,  .symb = CL_BR }, { .type = REDUCE, .num = 6U } },
   { {.state = 3U,  .symb = END   }, { .type = REDUCE, .num = 6U } },
 
   { {.state = 4U,  .symb = ID    }, { .type = SHIFT,  .num = 5U } },
+  { {.state = 4U,  .symb = NUM   }, { .type = SHIFT,  .num = 12U} }, //
   { {.state = 4U,  .symb = OP_BR }, { .type = SHIFT,  .num = 4U } },
 
   { {.state = 5U,  .symb = ADD   }, { .type = REDUCE, .num = 8U } },
+  { {.state = 5U,  .symb = SUB   }, { .type = REDUCE, .num = 8U } }, //
   { {.state = 5U,  .symb = MUL   }, { .type = REDUCE, .num = 8U } },
+  { {.state = 5U,  .symb = DIV   }, { .type = REDUCE, .num = 8U } }, //
   { {.state = 5U,  .symb = CL_BR }, { .type = REDUCE, .num = 8U } },
   { {.state = 5U,  .symb = END   }, { .type = REDUCE, .num = 8U } },
 
   { {.state = 6U,  .symb = ID    }, { .type = SHIFT,  .num = 5U } },
+  { {.state = 6U,  .symb = NUM   }, { .type = SHIFT,  .num = 12U} }, //
   { {.state = 6U,  .symb = OP_BR }, { .type = SHIFT,  .num = 4U } },
 
   { {.state = 7U,  .symb = ID    }, { .type = SHIFT,  .num = 5U } },
+  { {.state = 7U,  .symb = NUM   }, { .type = SHIFT,  .num = 12U} }, //
   { {.state = 7U,  .symb = OP_BR }, { .type = SHIFT,  .num = 4U } },
 
   { {.state = 8U,  .symb = ADD   }, { .type = SHIFT,  .num = 6U } },
+  { {.state = 8U,  .symb = SUB   }, { .type = SHIFT,  .num = 13U} }, //
   { {.state = 8U,  .symb = CL_BR }, { .type = SHIFT,  .num = 11U} },
 
   { {.state = 9U,  .symb = ADD   }, { .type = REDUCE, .num = 1U } },
+  { {.state = 9U,  .symb = SUB   }, { .type = REDUCE, .num = 1U } }, //
   { {.state = 9U,  .symb = MUL   }, { .type = SHIFT,  .num = 7U } },
+  { {.state = 9U,  .symb = DIV   }, { .type = SHIFT,  .num = 15U} }, //
   { {.state = 9U,  .symb = CL_BR }, { .type = REDUCE, .num = 1U } },
   { {.state = 9U,  .symb = END   }, { .type = REDUCE, .num = 1U } },
 
   { {.state = 10U, .symb = ADD   }, { .type = REDUCE, .num = 4U } },
+  { {.state = 10U, .symb = SUB   }, { .type = REDUCE, .num = 4U } }, //
   { {.state = 10U, .symb = MUL   }, { .type = REDUCE, .num = 4U } },
+  { {.state = 10U, .symb = DIV   }, { .type = REDUCE, .num = 4U } }, //
   { {.state = 10U, .symb = CL_BR }, { .type = REDUCE, .num = 4U } },
   { {.state = 10U, .symb = END   }, { .type = REDUCE, .num = 4U } },
 
   { {.state = 11U, .symb = ADD   }, { .type = REDUCE, .num = 7U } },
   { {.state = 11U, .symb = MUL   }, { .type = REDUCE, .num = 7U } },
   { {.state = 11U, .symb = CL_BR }, { .type = REDUCE, .num = 7U } },
-  { {.state = 11U, .symb = END   }, { .type = REDUCE, .num = 7U } }
+  { {.state = 11U, .symb = END   }, { .type = REDUCE, .num = 7U } },
+
+  { {.state = 12U, .symb = ADD   }, { .type = REDUCE, .num = 6U } }, //
+  { {.state = 12U, .symb = SUB   }, { .type = REDUCE, .num = 6U } }, //
+  { {.state = 12U, .symb = MUL   }, { .type = REDUCE, .num = 6U } }, //
+  { {.state = 12U, .symb = DIV   }, { .type = REDUCE, .num = 6U } }, //
+  { {.state = 12U, .symb = CL_BR }, { .type = REDUCE, .num = 6U } }, //
+  { {.state = 12U, .symb = END   }, { .type = REDUCE, .num = 6U } }, //
+
+  { {.state = 13U, .symb = ID    }, { .type = SHIFT,  .num = 5U } }, //
+  { {.state = 13U, .symb = NUM   }, { .type = SHIFT,  .num = 12U} }, //
+  { {.state = 13U, .symb = OP_BR }, { .type = SHIFT,  .num = 4U } }, //
+
+  { {.state = 14U, .symb = ADD   }, { .type = REDUCE, .num = 2U } }, //
+  { {.state = 14U, .symb = SUB   }, { .type = REDUCE, .num = 2U } }, //
+  { {.state = 14U, .symb = MUL   }, { .type = SHIFT,  .num = 7U } }, //
+  { {.state = 14U, .symb = DIV   }, { .type = SHIFT,  .num = 15U} }, //
+  { {.state = 14U, .symb = CL_BR }, { .type = REDUCE, .num = 2U } }, //
+  { {.state = 14U, .symb = END   }, { .type = REDUCE, .num = 2U } }, //
+
+  { {.state = 15U, .symb = ID    }, { .type = SHIFT,  .num = 5U } }, //
+  { {.state = 15U, .symb = NUM   }, { .type = SHIFT,  .num = 12U} }, //
+  { {.state = 15U, .symb = OP_BR }, { .type = SHIFT,  .num = 4U } }, //
+
+  { {.state = 16U, .symb = ADD   }, { .type = REDUCE, .num = 5U } },
+  { {.state = 16U, .symb = SUB   }, { .type = REDUCE, .num = 5U } }, //
+  { {.state = 16U, .symb = MUL   }, { .type = REDUCE, .num = 5U } },
+  { {.state = 16U, .symb = DIV   }, { .type = REDUCE, .num = 5U } }, //
+  { {.state = 16U, .symb = CL_BR }, { .type = REDUCE, .num = 5U } },
+  { {.state = 16U, .symb = END   }, { .type = REDUCE, .num = 5U } },
 };
 
 const Shift_reduce_parser::Goto_table Shift_reduce_parser::goto_table = {
 
-  { {.state = 0U, .symb = E}, 1U },
-  { {.state = 0U, .symb = T}, 2U },
-  { {.state = 0U, .symb = F}, 3U },
+  { {.state = 0U,  .symb = E}, 1U },
+  { {.state = 0U,  .symb = T}, 2U },
+  { {.state = 0U,  .symb = F}, 3U },
 
-  { {.state = 4U, .symb = E}, 8U },
-  { {.state = 4U, .symb = T}, 2U },
-  { {.state = 4U, .symb = F}, 3U },
+  { {.state = 4U,  .symb = E}, 8U },
+  { {.state = 4U,  .symb = T}, 2U },
+  { {.state = 4U,  .symb = F}, 3U },
 
-  { {.state = 6U, .symb = T}, 9U },
-  { {.state = 6U, .symb = F}, 3U },
+  { {.state = 6U,  .symb = T}, 9U },
+  { {.state = 6U,  .symb = F}, 3U },
 
-  { {.state = 7U, .symb = F}, 10U}
+  { {.state = 7U,  .symb = F}, 10U},
+
+  { {.state = 13U, .symb = T}, 14U}, //
+  { {.state = 13U, .symb = F}, 3U }, //
+
+  { {.state = 15U, .symb = F}, 16U}, //
 };
 
 void Shift_reduce_parser::parse(const tokens_vector& tokens) {
