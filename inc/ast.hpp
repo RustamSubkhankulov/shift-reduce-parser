97e38954fe6ac6ab113f7fb40a2f1096f14c7627
Rustam Subkhankulov <subkhankulov@ispras.ru>
2024-04-25 13:10:33 +0300

    started ast generating development

---
+++ inc/ast.hpp
@@ -0,0 +1,22 @@
+#ifndef AST_HPP
+#define AST_HPP
+
+#include <vector>
+
+class Ast_node;
+
+class Ast {
+
+};
+
+class Ast_node {
+
+  std::vector<Ast_node*> children;
+
+public:
+
+  
+
+};
+
+#endif // AST_HPP
